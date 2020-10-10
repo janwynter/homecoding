34b8d6b90b876a8be53b92c921d33c9143d2752e
janwynter <whitejane1212@gmail.com>
2020-10-10 20:30:56 +0900

    숫자 함수 추가

--- StudyPython/practice.py
+++ StudyPython/practice.py
@@ -1,7 +1,21 @@
-#Quiz) 변수를 이용하여 다음 문장을 출력하시오. 
-# 변수명 : station 
-# 변수 값 : "사당", "신도림", "인천공항" 순서대로 입력 
-# 출력문장: XX행 열차가 들어오고 있습니다. 
+# #숫자처리함수
+from math import *
+from random import *
 
-station = "사당"
-print(station, "행 열차가 들어오고 있습니다.")
+print("절대값 : ", abs(-5)) # 절대값 구하기 
+print("제곱 : ", pow(4,2)) # 4^2 
+print("최대값 : ", max(1,10)) # 최대값 구하기
+print("최소값 : ", min(1,10)) # 최소값 구하기
+
+
+print("반올림 : ", round(3.14)) # 반올림
+print("내림 : ", floor(3.14)) # 내림
+print("올림 : ", ceil(3.14)) # 올림
+print("제곱근 : ", sqrt(4)) # 제곱근 
+
+print("랜덤 숫자 : ", random()) # 0.0~1.0 미만 임의의 값 생성
+print("랜덤 숫자(int형) : ", int(random()))
+print("랜덤 숫자[1-45] : ", int(random()*45)+1)
+print("랜덤 숫자[1-45] : ", int(randint(1,45))) # 1~45이하 임의의 값 생성
+print("랜덤 숫자[1-45] : ", int(randrange(1,46))) #1~45미만 임의의 값 생성
+
